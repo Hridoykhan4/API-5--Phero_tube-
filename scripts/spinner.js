c4f7780258ace1d77a401ac66869f429b72218ef
Hridoykhan4 <hridoykhan148385@gmail.com>
2025-02-11 18:35:46 +0600

    categories and all videos loaded from API's

--- scripts/spinner.js
+++
@@ -1,9 +0,0 @@
-const loadSpinner = function (isLoading) {
-    const loadSpinner = document.getElementById('loadingSpinner');
-    if(isLoading){
-        loadSpinner.classList.remove('hidden')
-    }else{
-        loadSpinner.classList.add('hidden')
-
-    }
-}
--- scripts/video.js
+++ scripts/video.js
@@ -1,159 +1,88 @@
-// Convert Second to day
-const getTimeString = (time) => {
-  const day = parseInt(time / 86400);
-  let remainingSecond = time % 86400;
-
-  const hour = parseInt(remainingSecond / 3600);
-  remainingSecond = remainingSecond % 3600;
-
-  const min = parseInt(remainingSecond / 60);
-  remainingSecond = remainingSecond % 60;
-
-  return `${day} day ${hour} hour ${min} min ${remainingSecond} sec ago`;
-};
-
-// Show Category
 const loadCategories = async () => {
   const res = await fetch(
     "https://openapi.programming-hero.com/api/phero-tube/categories"
   );
   const data = await res.json();
-  displayCategory(data.categories);
+  showCategories(data.categories);
 };
 
-const displayCategory = (categories) => {
-  const categoryContainer = document.getElementById("categories");
-  categories.forEach((item) => {
-    // console.log(item);
-
-    const buttonContainer = document.createElement("div");
-    buttonContainer.innerHTML = `
-    <button id='btn-${item.category_id}' onclick="loadCategoriesVideos(${item.category_id})" class="btn category-btn">${item.category}</button>
-    `;
-    categoryContainer.append(buttonContainer);
-  });
-};
-
-const removeActiveClass = () => {
-  const buttons = document.getElementsByClassName("category-btn");
-  for (const btn of buttons) {
-    btn.classList.remove("active");
-  }
-};
-
-const loadCategoriesVideos = async (categoryId) => {
-  removeActiveClass();
-
-  const res = await fetch(
-    ` https://openapi.programming-hero.com/api/phero-tube/category/${categoryId}`
-  );
-  const data = await res.json();
-  const activeBtn = document.getElementById(`btn-${categoryId}`);
-  activeBtn.classList.add("active");
-  displayVideos(data.category);
-};
-
-const loadDetails = async (id) => {
-  const res = await fetch(
-    `https://openapi.programming-hero.com/api/phero-tube/video/${id}`
-  );
-  const data = await res.json();
-  displayDetails(data.video);
-};
-
-const displayDetails = (video) => {
-  console.log(video);
-  const detailContainer = document.getElementById("modal-content");
-  detailContainer.innerHTML = `
-      <img src="${video.thumbnail}">
-      <p>${video.description}</p>
-      `;
-
-  customModal.showModal();
-};
-
-//* Load Videos Section */
-const loadVideos = async (searchText = "") => {
-  const res = await fetch(
-    `https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`
-  );
-  const data = await res.json();
-  displayVideos(data.videos);
-};
-
-const displayVideos = (videos) => {
-  const videoContainer = document.getElementById("videos");
-  videoContainer.innerText = "";
-
-  if (videos.length === 0) {
-    videoContainer.classList.remove("grid");
-    videoContainer.innerHTML = `
-  <div class="min-h-[250px] w-full flex flex-col gap-5 justify-center items-center">
-  <img src="./assets/icon.png" />
-  <h2 class="text-xl font-bold">No Content Here in this category</h2>
-  </div>
-  `;
-    return;
-  } else {
-    videoContainer.classList.add("grid");
-  }
-
-  videos.forEach((video) => {
-    const card = document.createElement("div");
-    card.classList = `card card-compact`;
-    card.innerHTML = `
-        
-  <figure class="h-[200px] relative">
-    <img class="h-full w-full object-cover"
-      src="${video?.thumbnail}"
-      alt="Shoes" />
-    
-        ${
-          video?.others?.posted_date.length === 0
-            ? ""
-            : ` <span class="absolute right-2 bottom-2 bg-black text-white p-1 rounded text-xs">${getTimeString(
-                video?.others?.posted_date
-              )}</span>`
-        } 
-     
-     
-  </figure>
-  <div class="py-2 px-0 flex gap-2">
-        <div>
-            <img class="w-10 h-10 rounded-full object-cover" src="${
-              video?.authors[0]?.profile_picture
-            }" />
-        </div>
-        <div>
-        <div>
-        <h2 class="font-bold">${video.title}</h2>
-         </div>
-        <div class="flex gap-2 items-center">
-        <p class="text-gray-400">${video?.authors[0]?.profile_name}</p>
-        ${
-          video?.authors[0]?.verified === true
-            ? '<img class="w-5" src="./assets/tick.png"/>'
-            : ""
-        }
-        </div>
-        <p><button onclick="loadDetails('${
-          video?.video_id
-        }')" class="btn btn-sm btn-error text-white">Details</button></p>
-        </div>
-  </div>
-
-        `;
-
-    videoContainer.appendChild(card);
+const showCategories = (categories) => {
+  const categoriesContainer = document.getElementById("categories");
+  categories.forEach((category) => {
+    const button = document.createElement("button");
+    button.innerText = category.category;
+    button.className = `btn btn-error text-white`;
+    categoriesContainer.appendChild(button);
   });
   loadSpinner(false);
 };
 
-document.getElementById("search-input").addEventListener("keyup", (e) => {
-  loadVideos(e.target.value);
-});
+// Load Videos Section Start
+
+const loadVideos = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/phero-tube/videos"
+  );
+  const data = await res.json();
+  showVideos(data.videos)
+};
+
+/* {
+    "category_id": "1003",
+    "video_id": "aaai",
+    "thumbnail": "https://i.ibb.co/kc8CCFs/30-rock.png",
+    "title": "30 Rock",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/YZN9rQZ/tina.jpg",
+            "profile_name": "Tina Fey",
+            "verified": false
+        }
+    ],
+    "others": {
+        "views": "4.5K",
+        "posted_date": "14800"
+    },
+    "description": "'30 Rock,' led by Tina Fey, is a comedy series that has garnered 4.5K views. The show is a witty and humorous take on the behind-the-scenes antics of a fictional live comedy show. With its sharp writing and unforgettable characters, '30 Rock' is perfect for fans of smart, satirical humor and engaging storylines."
+} */
+
+
+const showVideos = (videos) => {
+  const videoContainer = document.getElementById('videos')
+  videos.forEach((video) => {
+    const card = document.createElement('div');
+    card.classList = `card card-compact bg-base-100`
+    card.innerHTML = `
+      <figure class="overflow-hidden h-[208px]">
+    <img
+      src="${video.thumbnail}" class="w-full h-full object-cover hover:scale-110 transition-all duration-300 ease-linear"
+      alt="Shoes" />
+  </figure>
+  <div class="py-3">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+    `
+    videoContainer.appendChild(card)
+  });
+  loadSpinner(false)
+}
+
+
+loadVideos()
+
+// Load Videos Section End
+
+const loadSpinner = (isLoading) => {
+  if (isLoading) {
+    document.getElementById("loadingSpinner").classList.remove("hidden");
+  } else {
+    document.getElementById("loadingSpinner").classList.add("hidden");
+  }
+};
+loadSpinner(true);
 
 loadCategories();
-setTimeout(() => {
-  loadVideos("");
-}, 1000);
